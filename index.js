10378fc531c852b7f4f9ad33d9256e0112dfc7be
Amber Frantz <amber.k.frantz@mgail.com>
2022-04-07 09:12:58 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // code your solution here
-function superbowlWin(array){
-    return array.find(element => element === "W");
+// function superbowlWin(array){
+//     return array.find(element => element === "W");
+// }
+
+
+function superbowlWin(array) {
+    function isWinningYear(year) {
+        return year.result === "W"
+    }
+    const winningYearObj = array.find(isWinningYear)
+
+    if (winningYearObj) {
+        return winningYearObj.year
+    }
 }
